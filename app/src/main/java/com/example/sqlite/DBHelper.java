82832167683686d8f7f6dc1781eccda7d988a253
Pavel Nagornev <pavel.nagornev@mail.ru>
2021-10-11 22:56:31 +0300

    review_code

--- app/src/main/java/com/example/sqlite/DBHelper.java
+++ app/src/main/java/com/example/sqlite/DBHelper.java
@@ -6,12 +6,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
     public static final  int DATABASE_VERSION =1;
-    public static final String DATABASE_NAME = "contactDb";
-    public static final String TABLE_CONTACTS = "contacts";
+    public static final String DATABASE_NAME = "teaDb";
+    public static final String TABLE_TEA = "tea";
 
     public static final String KEY_ID = "_id";
     public static final String KEY_NAME = "name";
-    public static final String KEY_MAIL = "mail";
+    public static final String KEY_COUNTRY = "country";
+    public static final String KEY_TASTE = "taste";
+    public static final String KEY_PRICE = "price";
 
     public  DBHelper(Context context)
     {
@@ -19,17 +21,19 @@ public class DBHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_CONTACTS + "(" +
+        db.execSQL("create table " + TABLE_TEA + "(" +
                 KEY_ID + " integer primary key," +
                 KEY_NAME + " text," +
-                KEY_MAIL + " text" + ")");
+                KEY_COUNTRY + " text," +
+                KEY_TASTE + " text," +
+                KEY_PRICE + " text" +")");
 
     }
 
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_CONTACTS);
+        db.execSQL("drop table if exists " + TABLE_TEA);
 
         onCreate(db);
 
--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -2,24 +2,33 @@ package com.example.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.DatabaseErrorHandler;
 import android.os.Bundle;
 import android.view.View;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnAdd, btnRead, btnClear;
-    EditText etName, etEmail;
+    Button btnAdd, btnClear;
+    EditText etName, etCountry, etTaste, etPrice;
 
-    TextView textRes;
 
     DBHelper dbHelper;
+
+    SQLiteDatabase database;
+
+    ContentValues contentValues;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,63 +38,160 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(this);
 
-        btnRead = (Button) findViewById(R.id.btnRead);
-        btnRead.setOnClickListener(this);
+
 
         btnClear = (Button) findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
 
         etName = (EditText) findViewById(R.id.etName);
-        etEmail = (EditText) findViewById(R.id.etEmail);
+        etCountry = (EditText) findViewById(R.id.etCountry);
+        etTaste = (EditText) findViewById(R.id.etTaste);
+        etPrice = (EditText) findViewById(R.id.etPrice);
 
-        textRes = (TextView) findViewById(R.id.textRes);
 
         dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+        UpdateTable();
+
     }
+
+
+
+    public  void  UpdateTable(){
+        Cursor cursor = database.query(DBHelper.TABLE_TEA, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+            int countryIndex = cursor.getColumnIndex(DBHelper.KEY_COUNTRY);
+            int tasteIndex = cursor.getColumnIndex(DBHelper.KEY_TASTE);
+            int priceIndex = cursor.getColumnIndex(DBHelper.KEY_PRICE);
+
+            TableLayout dbOutput = findViewById(R.id.dbtable);
+            dbOutput.removeAllViews();
+            do{
+                TableRow dbOutputRow = new TableRow(this);
+                dbOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+                TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+                TextView outputID = new TextView(this);
+                params.weight =1.0f;
+                outputID.setLayoutParams(params);
+                outputID.setText(cursor.getString(idIndex));
+                dbOutputRow.addView(outputID);
+
+                TextView outputName = new TextView(this);
+                params.weight =1.0f;
+                outputName.setLayoutParams(params);
+                outputName.setText(cursor.getString(nameIndex));
+                dbOutputRow.addView(outputName);
+
+                TextView outputCountry = new TextView(this);
+                params.weight =1.0f;
+                outputCountry.setLayoutParams(params);
+                outputCountry.setText(cursor.getString(countryIndex));
+                dbOutputRow.addView(outputCountry);
+
+                TextView outputTaste = new TextView(this);
+                params.weight =1.0f;
+                outputTaste.setLayoutParams(params);
+                outputTaste.setText(cursor.getString(tasteIndex));
+                dbOutputRow.addView(outputTaste);
+
+                TextView outputPrice = new TextView(this);
+                params.weight =1.0f;
+                outputPrice.setLayoutParams(params);
+                outputPrice.setText(cursor.getString(priceIndex));
+                dbOutputRow.addView(outputPrice);
+
+                Button btnDelete = new Button(this);
+                btnDelete.setOnClickListener(this);
+                params.weight = 1.0f;
+                btnDelete.setLayoutParams(params);
+                btnDelete.setText("Удалить запись");
+                btnDelete.setId(cursor.getInt(idIndex));
+                dbOutputRow.addView(btnDelete);
+
+                dbOutput.addView(dbOutputRow);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+    }
+
+
+
 
     @Override
     public void onClick(View v) {
 
-        String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
-
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-
-
+        database = dbHelper.getWritableDatabase();
         switch (v.getId()) {
 
             case R.id.btnAdd:
+
+                String name = etName.getText().toString();
+                String country = etCountry.getText().toString();
+                String taste = etTaste.getText().toString();
+                String price = etPrice.getText().toString();
+
+
+                contentValues = new ContentValues();
                 contentValues.put(DBHelper.KEY_NAME, name);
-                contentValues.put(DBHelper.KEY_MAIL, email);
+                contentValues.put(DBHelper.KEY_COUNTRY, country);
+                contentValues.put(DBHelper.KEY_TASTE, taste);
+                contentValues.put(DBHelper.KEY_PRICE, price);
 
-                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
+                database.insert(DBHelper.TABLE_TEA, null, contentValues);
 
+                etName.setText("");
+                etCountry.setText("");
+                etTaste.setText("");
+                etPrice.setText("");
+                UpdateTable();
                 break;
 
-            case R.id.btnRead:
-                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-                String text = "";
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    do {
-                        text = text + "ID = " + String.valueOf(cursor.getInt(idIndex)) +
-                                ", name = " + String.valueOf(cursor.getString(nameIndex)) +
-                                ", email = " + String.valueOf(cursor.getString(emailIndex)) + "\n";
-                    } while (cursor.moveToNext());
-                    Log.d("mLog",text);
-                    textRes.setText(text);
-                } else
-                    Log.d("mLog","0 rows");
 
-                cursor.close();
-                break;
 
             case R.id.btnClear:
-                database.delete(DBHelper.TABLE_CONTACTS, null, null);
+                database.delete(DBHelper.TABLE_TEA, null, null);
+                UpdateTable();
+                break;
+
+            default:
+                database.delete(DBHelper.TABLE_TEA,DBHelper.KEY_ID +" = ?", new String[]{String.valueOf(v.getId())});
+                UpdateTable();
+
+                contentValues = new ContentValues();
+
+                Cursor cursorUdater = database.query(DBHelper.TABLE_TEA, null, null, null, null, null, null);
+                if (cursorUdater.moveToFirst()) {
+                    int idIndex = cursorUdater.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursorUdater.getColumnIndex(DBHelper.KEY_NAME);
+                    int countryIndex = cursorUdater.getColumnIndex(DBHelper.KEY_COUNTRY);
+                    int tasteIndex = cursorUdater.getColumnIndex(DBHelper.KEY_TASTE);
+                    int priceIndex = cursorUdater.getColumnIndex(DBHelper.KEY_PRICE);
+                    int realId = 1;
+                    do{
+                        if(cursorUdater.getInt(idIndex)>realId);
+                        {
+                            contentValues.put(DBHelper.KEY_ID, realId);
+                            contentValues.put(DBHelper.KEY_NAME, cursorUdater.getString(nameIndex));
+                            contentValues.put(DBHelper.KEY_COUNTRY, cursorUdater.getString(countryIndex));
+                            contentValues.put(DBHelper.KEY_TASTE, cursorUdater.getString(tasteIndex));
+                            contentValues.put(DBHelper.KEY_PRICE, cursorUdater.getString(priceIndex));
+                            database.replace(DBHelper.TABLE_TEA, null, contentValues);
+
+                        }
+                        realId++;
+                    }while (cursorUdater.moveToNext());
+                    if(cursorUdater.moveToLast())
+                    {
+                        database.delete(DBHelper.TABLE_TEA, DBHelper.KEY_ID + " = ?", new String[]{cursorUdater.getString(idIndex)});
+                    }
+                }
+                cursorUdater.close();
+                UpdateTable();
                 break;
         }
         dbHelper.close();
